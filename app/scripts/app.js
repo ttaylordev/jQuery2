6ab56dfe769f7e2e4fbebeb9398e6c4ed0349cbf
Tyson Taylor <mellowt@gmail.com>
2015-12-10 15:10:54 -0700

    needs work, low priority

---
+++ app/scripts/app.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+  var listo = [];
+
+var Task = function(task) {
+  this.task = task;
+  this.id = 'new';
+};
+var advanceTask = function(task) {
+  var modified = task.innerText.trim();
+  for (var i = 0; i < listo.length; i++) {
+    if (listo[i].task === modified) {
+      if (listo[i].id === 'new') {
+        listo[i].id = 'inProgress';
+      } else if (listo[i].id === 'inProgress') {
+        listo[i].id = 'archived';
+      } else {
+        listo.splice(i, 1);
+      }
+      break;
+    }
+  }
+  task.remove();
+};
+$(document).on('click', '#item', function(e) {
+	e.preventDefault();
+  var task = this;
+  advanceTask(task);
+  this.id = 'inProgress';
+});
+$(document).on('click', '#item', function(e) {
+	e.preventDefault();
+});
+$(document).on('click', '#item', function(e) {
+	e.preventDefault();
+  var task = this;
+  advanceTask(task);
+  this.id = 'inProgress';
+  $('#currentList').append(this.outerHTML);
+});
+$(document).on('click', '#inProgress', function (e) {
+  e.preventDefault();
+  var task = this;
+  task.id = "archived";
+  var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+  advanceTask(task);
+  $('#archivedList').append(changeIcon);
+});
+$(document).on('click', '#archived', function (e) {
+  e.preventDefault();
+  var task = this;
+  advanceTask(task);
+});
+
+$('newTaskForm').hide();
+var addTask = function(task) {
+  if(task) {
+    task = new Task(task);
+    listo.push(task);
+  }
+  $('#newItemInput').val('');
+
+  $('#newList').append('<a href="#" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
+
+  $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+  $('#saveNewItem').on('click', function (e) {
+      e.preventDefault();
+      var task = $('#newItemInput').val().trim();
+      addTask(task);
+  });
+  $('#newListItem').on('click', function () {
+      $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+  });
+  //closes form
+  $('#cancel').on('click', function (e) {
+      e.preventDefault();
+      $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+  });
+
+};
+
+
+
+});
+
+
+// provide a backgraound image loading tool so that people can pixilate any image
+// provide a dynamic color selector instead of static colors. allow settings, 8-bit, 16-bit, 32-bit, or 64-bit color schemes
+// ideally anyone could essentially trace over their favorite characters and images to transform them into 8 bit characters.
+// maybe have a button that automatically 8-bits images.
